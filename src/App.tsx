4dd974d02204a91c3056c856f23da55e0a7bc050
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-03-21 13:05:45 -0300

    feat:  avoiding and showing messages on the screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,110 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData, } from 'react-firebase-hooks/firestore';
+
+import firebase, { auth, firestore } from './services/firebase';
+
 import './App.css';
 
 export default function App() {
+  const [user] = useAuthState(auth);
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Super <code>chat</code>.
-        </p>
+      <header>
+        <SignOut />
       </header>
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
 }
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return (
+    <div>
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
+    </div>
+  );
+}
+
+function SignOut() {
+  const handleSignOut = () => {
+    auth.signOut();
+  };
+
+  return (
+    auth.currentUser && (
+      <div>
+        <button onClick={handleSignOut}>Sign Out</button>
+      </div>
+    )
+  );
+}
+
+function ChatRoom() {
+  const dummy = useRef<null | HTMLDivElement>(null);
+
+  const [formValue, setFormValue] = useState('');
+
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(30);
+
+  const [messages] = useCollectionData(query, { idFiekd: 'id' });
+
+  const handleChangeInput = (event: React.FormEvent<HTMLInputElement>) => {
+    setFormValue(event.currentTarget.value);
+  }
+
+  const handleSendMessage = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid: auth.currentUser?.uid ?? null,
+      photoURL: auth.currentUser?.photoURL ?? ''
+    })
+
+    setFormValue('');
+
+    dummy.current?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (
+    <div>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+
+        <div ref={dummy}></div>
+      </main>
+
+      <form onSubmit={handleSendMessage}>
+        <input value={formValue} onChange={handleChangeInput} />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
+}
+
+interface IChatMessage {
+  key: number;
+  message: firebase.firestore.DocumentData;
+}
+
+function ChatMessage(props: IChatMessage) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser?.uid ? 'send' : 'received';
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} alt={'user-logo'} />
+      <p>{text}</p>
+    </div>
+  );
+};
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,18 +2,18 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const auth = firebase.auth();
-const firestore = firebase.firestore();
-
 firebase.initializeApp({
-  apiKey: "AIzaSyAM2MWUt3G3IfNRaEvI1DYwHmLALES5FYI",
+  apiKey: process.env.REACT_APP_FIREBASE_API,
   authDomain: "super-chat-3f0a9.firebaseapp.com",
   projectId: "super-chat-3f0a9",
   storageBucket: "super-chat-3f0a9.appspot.com",
-  messagingSenderId: "604109296753",
-  appId: "1:604109296753:web:b874b208a707326d45f7d0",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: "G-3B997LC8DF"
 });
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 export default firebase;
 export { auth, firestore };
